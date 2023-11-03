5c9331da16f102d7285b03cf7429c455f0fbf174
K Stephan Torres <kevinstephantorres@gmail.com>
2023-11-03 12:41:48 -0300

    Se agrego validaciones

--- js/punto7.js
+++ js/punto7.js
@@ -1,8 +1,15 @@
 let cantidad= parseInt(prompt('Ingrese un numero no mayor a 50: '))
-
-for(let i=cantidad;i>0;i--){
-    for(let j=0;j<i;j++){
-        document.write(i)
+if(!isNaN(cantidad)){
+    if(cantidad<=50 && cantidad>=1){
+        for(let i=cantidad;i>0;i--){
+            for(let j=0;j<i;j++){
+                document.write(i)
+            }
+            document.write(`<br>`)
+        }
+    }else{
+        alert("El número ingresado está fuera de los parámetros establecidos")
     }
-    document.write(`<br>`)
+}else{
+    alert('Debe ingresar un número')
 }
